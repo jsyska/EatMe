8450d08477b9c1de66db3f89fb94a71b7ad662c8
Jakub Syska <jaksyska@gmail.com>
2021-08-11 00:22:41 +0200

    added modal via react portal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>Eat.me</h1>
-            <HeaderCartButton></HeaderCartButton>
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="Delicous Food!" />
